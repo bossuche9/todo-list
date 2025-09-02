4e402def0e9c6da31f21a710d11a2f5d314ca7fe
bossuche9 <uchennaobi102@gmail.com>
2025-09-02 14:01:08 -0400

    feat: added functionality to the buttons in TodoViewForm.jsx by assing the props value in the instance for the Onchange and vlaue of Sort by and Direction labels.

--- src/features/TodosViewForm.jsx
+++ src/features/TodosViewForm.jsx
@@ -10,7 +10,10 @@ function TodoViewForm({
         <label>
           {' '}
           Sort by
-          <select onChange={(event) => setSortField(event)} value={sortField}>
+          <select
+            onChange={(event) => setSortField(event.target.value)}
+            value={sortField}
+          >
             <option value="title"> Title</option>
             <option value="createdTime">Time added</option>
           </select>
@@ -18,7 +21,10 @@ function TodoViewForm({
         <label>
           {' '}
           Direction
-          <select>
+          <select
+            value={sortDirection}
+            onChange={(e) => setSortDirection(e.target.value)}
+          >
             <option value="asc">Ascending</option>
             <option value="desc">Descending</option>
           </select>
