a239758bd12c724ed136304fef2f0060d84fbf40
bossuche9 <uchennaobi102@gmail.com>
2025-09-09 17:38:44 -0400

    feat: imported useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 import { useEffect, useState } from 'react';
 import TodosViewForm from './features/TodosViewForm';
+import { useCallback } from 'react';
 
 const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
 
